f6ebe9766f79b6f133bfa07c89eba55aa9c94d33
anngiepp <angie.portocarrero@tecsup.edu.pe>
2026-06-18 13:18:07 -0500

    chore: ajustar compileSdk para dependencias androidX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.plant.forestcare"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.plant.forestcare"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
